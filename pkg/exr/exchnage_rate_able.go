b81090487683a156db0ca8abc656d2aabaef09ce
fullgukbap <2hanjunbum6@gmail.com>
2024-05-03 16:34:36 +0900

    fix: 매매기준율 함수 이름 변경

--- pkg/exr/exchnage_rate_able.go
+++ pkg/exr/exchnage_rate_able.go
@@ -2,7 +2,7 @@ package exr
 
 // ExchnageRateAble 인터페이스는 환율 정보를 얻는 행위들을 정의합니다.
 type exchnageRateAble interface {
-	// GetTradeThreshold 함수는 통화코드(currencyCode)에 해당되는 매매 기준율을 반환홥니다.
+	// GetDealBaseRate 함수는 통화코드(currencyCode)에 해당되는 매매 기준율을 반환홥니다.
 	// 2024.05.03 기준으로 지원한 통화코드는 미국, 일본, 호주, 캐나다 입니다.
-	GetTradeThreshold(currencyCode string) (float64, error)
+	GetDealBaseRate(currencyCode string) (float64, error)
 }
